e8fca48d145391b9563dfbb77bec231db3aa439c
neoazubal <valentinfamery087@gmail.com>
2023-06-14 18:12:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/template_android_kotlin_compose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.template_android_kotlin_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.template_android_kotlin_compose.ui.theme.Template_android_kotlin_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Template_android_kotlin_composeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Template_android_kotlin_composeTheme {
+        Greeting("Android")
+    }
+}
